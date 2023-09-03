20db9c60233a7cc60bc528c53e7190d9beba6472
Alexey Peremyshlennikov <adambrn@mail.ru>
2023-09-03 20:41:31 +0700

    Edit models urls end create base.html

--- lesson2/lesson2/urls.py
+++ lesson2/lesson2/urls.py
@@ -21,14 +21,17 @@ from task1 import views
 urlpatterns = [
     path("admin/", admin.site.urls),
 
+    # URL для home
+    path('', views.index, name='index'),
+
     # URL для создания клиента
     path('client/create/', views.create_client, name='create_client'),
 
+    # URL для просмотра клиента
+    path('client/<int:client_id>/', views.client_detail, name='client_detail'),
+
     # URL для просмотра списка клиентов
     path('clients/', views.client_list, name='client_list'),
-
-    # URL для просмотра информации о клиенте
-    path('client/<int:client_id>/', views.client_detail, name='client_detail'),
 
     # URL для обновления клиента
     path('client/<int:client_id>/update/', views.update_client, name='update_client'),
@@ -39,11 +42,11 @@ urlpatterns = [
     # URL для создания товара
     path('product/create/', views.create_product, name='create_product'),
 
+    # URL для просмотра товара
+    path('product/<int:product_id>', views.product_detail, name='product_detail'),
+
     # URL для просмотра списка товаров
     path('products/', views.product_list, name='product_list'),
-
-    # URL для просмотра информации о товаре
-    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 
     # URL для обновления товара
     path('product/<int:product_id>/update/', views.update_product, name='update_product'),
@@ -54,11 +57,11 @@ urlpatterns = [
     # URL для создания заказа
     path('order/create/', views.create_order, name='create_order'),
 
+    # URL для просмотра заказа
+    path('order/<int:order_id>/', views.order_details, name='order_details')
+
     # URL для просмотра списка заказов
     path('orders/', views.order_list, name='order_list'),
-
-    # URL для просмотра информации о заказе
-    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
 
     # URL для обновления заказа
     path('order/<int:order_id>/update/', views.update_order, name='update_order'),
