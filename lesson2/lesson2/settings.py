8d7a1ec827eae173db70aa835e2206c74870a70b
Alexey Peremyshlennikov <adambrn@mail.ru>
2023-09-15 18:25:59 +0700

    server settings

--- lesson2/lesson2/settings.py
+++ lesson2/lesson2/settings.py
@@ -21,12 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t!wz)!j=pv*=y=*2mah(vn!5%uj^1c#5gr32am1-%g*a2l1f&1"
+#SECRET_KEY = "django-insecure-t!wz)!j=pv*=y=*2mah(vn!5%uj^1c#5gr32am1-%g*a2l1f&1"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'adambrn.pythonanywhere.com',]
 
 
 # Application definition
@@ -75,13 +77,22 @@ WSGI_APPLICATION = "lesson2.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'adambrn$default',
+        'USER': 'adambrn',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'adambrn.mysql.pythonanywhere-services.com',
+        'OPTIONS': {'init_command': "SET NAMES 'utf8mb4; SET sql_mode='STRICT_TRANS_TABLES'", 'charset': 'utf8mb4',},
+        }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -117,9 +128,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static/'
+#MEDIA_URL = 'media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
