6a86e491307981bc41637f32a28ae114aa233573
Alexey Peremyshlennikov <adambrn@mail.ru>
2023-09-15 17:59:49 +0700

    lesson5

--- lesson2/task1/admin.py
+++ lesson2/task1/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from models import Client, Order, Product
+from .models import Client, Order, Product
 # Register your models here.
 class ClientAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ["name"]
+
 class OrderAdmin(admin.ModelAdmin):
     pass
+
 class ProductAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ["name"]
 
 admin.site.register(Client, ClientAdmin)
 admin.site.register(Order, OrderAdmin)
