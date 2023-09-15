64dcfdf03a401f9015b77d521cd8fa6ac5ef24d2
Alexey Peremyshlennikov <adambrn@mail.ru>
2023-09-15 07:16:43 +0700

    add model admin

--- lesson2/task1/admin.py
+++ lesson2/task1/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from models import Client, Order, Product
 # Register your models here.
+class ClientAdmin(admin.ModelAdmin):
+    pass
+class OrderAdmin(admin.ModelAdmin):
+    pass
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Product, ProductAdmin)
+
+
